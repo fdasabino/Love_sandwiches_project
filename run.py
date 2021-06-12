8a065742cf7712860a91baadf95f2e5f0010c0c7
Francisco Sabino <fdasabino@gmail.com>
2021-06-12 11:56:59 +0200

    Create get sales data function to get data sring form the user

--- run.py
+++ run.py
@@ -18,12 +18,12 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market")
+    print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    print("Example: 10, 20, 30, 40, 50, 60\n")
     
     data_str = input("Enter your data here: ")
     print(f"The data provided is {data_str}")
     
-get_sales_data()
 
+get_sales_data()
